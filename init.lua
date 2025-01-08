08a57faed7f73d4efb57989f65ba621ee41553f8
Tatsuya Kamijo <tatsukamijo@icloud.com>
2025-01-09 04:01:50 +0900

    change indent width depending on the language

--- init.lua
+++ init.lua
@@ -129,6 +129,27 @@ end)
 -- Enable break indent
 vim.opt.breakindent = true
 
+-- indent width depending on filetype
+local filetype_tabstop = {
+  lua = 2,
+  markdown = 2,
+  python = 4, -- Python indent width
+  c = 2, -- C indent width
+  cpp = 2, -- C++ indent width
+}
+
+local usrftcfg = vim.api.nvim_create_augroup('UserFileTypeConfig', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  group = usrftcfg,
+  callback = function(args)
+    local ftts = filetype_tabstop[args.match]
+    if ftts then
+      vim.bo.tabstop = ftts
+      vim.bo.shiftwidth = ftts
+    end
+  end,
+})
+
 -- Save undo history
 vim.opt.undofile = true
 
