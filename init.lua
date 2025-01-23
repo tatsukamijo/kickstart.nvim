e03611945c543093dff5875546970c4b2f3410f4
Tatsuya Kamijo <tatsukamijo@icloud.com>
2025-01-23 19:21:52 +0900

    add markdown preview

--- init.lua
+++ init.lua
@@ -985,6 +985,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.vim-tmux-navigator',
+  require 'custom.plugins.markdown',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,11 @@
+-- markdown-preview
+-- https://github.com/iamcco/markdown-preview.nvim
+--
+return {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
+}
