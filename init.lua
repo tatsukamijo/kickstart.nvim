6aacc239229adef96541786378da4df3f44dfccd
Tatsuya Kamijo <tatsukamijo@icloud.com>
2026-01-01 20:39:45 +0900

    fix: suppress mason error

--- init.lua
+++ init.lua
@@ -711,6 +711,7 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        automatic_enable = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
