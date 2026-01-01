0f046699cb16dd6d7f5d795de04e9953c76d6cd9
Tatsuya Kamijo <tatsukamijo@icloud.com>
2026-01-01 23:12:21 +0900

    feat: enable tmux navigator in small terminal

--- lua/custom/plugins/vim-tmux-navigator.lua
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -3,24 +3,17 @@
 --
 return {
   'christoomey/vim-tmux-navigator',
-  cmd = {
-    'TmuxNavigateLeft',
-    'TmuxNavigateDown',
-    'TmuxNavigateUp',
-    'TmuxNavigateRight',
-    'TmuxNavigatePrevious',
-    'TmuxNavigatorProcessList',
-  },
-  keys = {
-    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-    -- Terminal mode navigation
-    { '<c-h>', '<C-\\><C-n><cmd>TmuxNavigateLeft<cr>', mode = 't' },
-    { '<c-j>', '<C-\\><C-n><cmd>TmuxNavigateDown<cr>', mode = 't' },
-    { '<c-k>', '<C-\\><C-n><cmd>TmuxNavigateUp<cr>', mode = 't' },
-    { '<c-l>', '<C-\\><C-n><cmd>TmuxNavigateRight<cr>', mode = 't' },
-  },
+  lazy = false,
+  config = function()
+    -- Normal mode
+    vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<cr>')
+    vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<cr>')
+    vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<cr>')
+    vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<cr>')
+    -- Terminal mode
+    vim.keymap.set('t', '<C-h>', '<C-\\><C-n><cmd>TmuxNavigateLeft<cr>')
+    vim.keymap.set('t', '<C-j>', '<C-\\><C-n><cmd>TmuxNavigateDown<cr>')
+    vim.keymap.set('t', '<C-k>', '<C-\\><C-n><cmd>TmuxNavigateUp<cr>')
+    vim.keymap.set('t', '<C-l>', '<C-\\><C-n><cmd>TmuxNavigateRight<cr>')
+  end,
 }
